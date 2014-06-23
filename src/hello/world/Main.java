a9aebbfe8c0c387683614cd4ec0b45b766201ef6
eric <eric@eric.net>
2014-06-23 06:10:28 -0400

    BufferedReaders

---
+++ src/hello/world/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+package hello.world;
+
+import java.io.*;
+
+/**
+ * @author eric
+ *
+ */
+public final class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(reader.readLine());
+	}
+
+}
