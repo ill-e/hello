a540ec645e5fbea78f41fe6005b6ce186bac4863
eric <eric@laptop>
2014-06-23 08:51:16 -0400

    java.util.Scanner

--- src/hello/world/Main.java
+++ src/hello/world/Main.java
@@ -5,6 +5,7 @@
 package hello.world;
 
 import java.io.*;
+import java.util.*;
 
 /**
  * @author eric
@@ -12,9 +13,15 @@ import java.io.*;
  */
 public final class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println(r.readLine());
+	public static void main(String[] args) {
+		// BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+		Scanner s = null;
+		try {
+			s = new Scanner(System.in);
+			System.out.println(s.nextLine());
+		} finally {
+			if (s != null) s.close();
+		}
 	}
 
 }
